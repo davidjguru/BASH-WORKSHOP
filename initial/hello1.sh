b188c7f92ad906b7b375981c1248796a171d5430
davidjguru <davidjguru@gmail.com>
2021-10-12 13:40:23 +0200

    (BSH) - Add two initial scripts.

---
+++ initial/hello1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+#
+#
+#-----------------------------------------------------------------------------------
+# 
+# Just a single Hello World 
+# [David Rodríguez, @davidjguru] [davidjguru@gmail.com]
+# Title: Hello World Bash Script
+# Description: First script echoing a simple Hello World as first exercise for workshop. 
+#
+# davidjguru@gmail.com
+# https://davidjguru.github.io
+# https://www.therussianlullaby.com
+#-----------------------------------------------------------
+#
+echo "Hello World" # Write to STDOUT standard output.
+exit 0 # Status code basic exit value without errors in execution.
---
+++ initial/hello2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+#
+#
+#-----------------------------------------------------------------------------------
+# 
+# Just a single Hello World using parameters from prompt. 
+# [David Rodríguez, @davidjguru] [davidjguru@gmail.com]
+# Title: Hello World Bash Script V2
+# Description: Second script echoing a simple Hello World using parameters as second exercise. 
+#
+# davidjguru@gmail.com
+# https://davidjguru.github.io
+# https://www.therussianlullaby.com
+#-----------------------------------------------------------
+#
+echo "Hello, Dear $1" # Write to STDOUT standard output.
+exit 0 # Status code basic exit value without errors in execution.
