f9824c9203c0019865fc539b3ac0c71f7fb3e397
davidjguru <davidjguru@gmail.com>
2021-10-24 01:11:36 +0200

    (BSH) - Adds more functions for loading strings and messages.

--- 04. Utilities/druclinic.sh
+++ 04. Utilities/druclinic.sh
@@ -30,6 +30,27 @@ load_colors () {
   NC='\033[0m' # No Color.
 }
 
+# Load a set of messages. 
+load_messages () {
+  STEP_ONE="    ${CYAN}STEP ONE: GETTING INFO ABOUT CONFIG FILES, CONTENT TYPES AND PARAGRAPHS.${NC}\n"
+  CONFIG_OPERATIONS="    ${CYAN}-----------------------------------------------------------------------------------------------\n\n
+    After initial loads, we're going to search the config files looking for related info...\n\n\
+    ----------------------------------------------------------------------------------------------- \n\n${NC}"
+
+}
+
+# Print selected messages. 
+print_messages () {
+  if [ $1 = "S1" ]
+    then
+       echo -e "${STEP_ONE}"
+  elif [ $1 = "c" ] 
+    then   
+      echo -e "${CONFIG_OPERATIONS}"
+      sleep 2
+  fi
+} 
+
 # Print the initial Splash in console.
 print_splash () {
   echo -e  "\n\n\n\n"
@@ -91,10 +112,21 @@ counting_config_files () {
     then
       config_files=$(ls -lah $config_path 2> /dev/null | wc -l)
       content_types=$(ls -lah $config_path/node.type.* 2> /dev/null | wc -l)
-      paragraphs_items=$(ls -lah $config_path/paragraphs.paragraphs_type.*  2> /dev/null | wc -l)
+      paragraph_items=$(ls -lah $config_path/paragraphs.paragraphs_type.*  2> /dev/null | wc -l)
+      block_items=$(ls -lah $config_path/block.block.*  2> /dev/null | wc -l)
+      vocabulary_items=$(ls -lah $config_path/taxonomy.vocabulary*  2> /dev/null | wc -l)
+      field_items=$(ls -lah $config_path/field.storage*  2> /dev/null | wc -l)
+      view_items=$(ls -lah $config_path/views.view*  2> /dev/null | wc -l)
+      user_roles=$(ls -lah $config_path/user.role*  2> /dev/null | wc -l)
       echo -e " ${CYAN} Number of config files:${NC} $config_files" 
       echo -e " ${CYAN} Number of content types:${NC} $content_types"
-      echo -e " ${CYAN} Number or paragraphs:${NC} $paragraphs_items"
+      echo -e " ${CYAN} Number of paragraphs:${NC} $paragraph_items"
+      echo -e " ${CYAN} Number of existing blocks:${NC} $block_items"
+      echo -e " ${CYAN} Number of vocabularies:${NC} $vocabulary_items"
+      echo -e " ${CYAN} Number of existing fields:${NC} $field_items"
+      echo -e " ${CYAN} Number of Views:${NC} $view_items"
+      echo -e " ${CYAN} Number of User Roles:${NC} $user_roles"
+      echo -e ""
       echo -e  "\n\n\n\n"
   else
     echo -e " ${ORANGE} Warning:${NC} We could not find any configuration files, the usual addresses do not exist.\n\n"
@@ -137,12 +169,12 @@ calculating_space_use () {
 }
 
 # Calling functions zone
+# Initial load functions.
 load_colors
+load_messages
 print_splash
-echo -e "-----------------------------------------------------------------------------------------------"
-echo -e " ${CYAN} After initial loads, we're going to inspect info about config files and content types...${NC}\n\n\n"
-echo -e "-----------------------------------------------------------------------------------------------"
-sleep 2
+print_messages S1
+print_messages c
 counting_config_files
 echo -e " ${CYAN} After initial loads, we're going to inspect info about config files and content types...\n\n\n"
 sleep 2
