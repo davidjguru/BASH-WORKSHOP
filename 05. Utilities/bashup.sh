aa20c75ec39ad2e2ce2ba323413846eebcafdae7
davidjguru <davidjguru@gmail.com>
2021-11-08 20:40:52 +0100

    (BSH) - Allow select compressed format for the backup.

--- 05. Utilities/bashup.sh
+++ 05. Utilities/bashup.sh
@@ -18,8 +18,9 @@
 # Last Edited: November 04 2021
 
 # Take input from console.
-read -p "Which file type to backup? Please introduce extension..." file_suffix
-read -p "Which folder to backup to? Please introduce destiny..." dir_name
+read -p "Which file type to backup? Please introduce extension: " file_suffix
+read -p "Which folder to backup to? Please introduce destiny: " dir_name
+read -p "Select the compression format [tar.gz, tar, gz, zip]: " compression
 
 # Create directory if not exists.
 destiny_path=$HOME/$dir_name
@@ -28,6 +29,25 @@ test -d $destiny_path || mkdir -m 700 $destiny_path
 # Execute search matching criteria and purging the same backup folder.
 find $HOME -path $destiny_path -prune -o -name "*$file_suffix" \
 -exec cp {} $destiny_path/ \;
+
+# Execute the compression operation.
+cd $HOME
+case "$compression" in 
+  tar.gz )
+    tar -czvf $dir_name.tar.gz $destiny_path
+    ;;
+  tar )
+    tar -cvf $dir_name.tar $destiny_path
+    ;;
+  gz )
+    gzip -9 -r $destiny_path
+    ;;
+  zip )
+    zip -r $dir_name.zip $destiny_path
+    ;;
+  *)
+    echo "Sorry but your input wasn't a valid option."
+esac
 
 # Gives a summary for the backup works.
 echo -e "\nWere copied $(ls -la $destiny_path | wc -l) files in $destiny_path."
