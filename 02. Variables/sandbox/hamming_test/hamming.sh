b10afd72bffbacf06cccf1a53aff1361ad29a737
David Rodrguez <davidjguru@gmail.com>
2021-11-14 20:53:23 +0100

    (BSH) - Add a new clausule for passing some tests.

--- 02. Variables/sandbox/hamming_test/hamming.sh
+++ 02. Variables/sandbox/hamming_test/hamming.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -z $1 ]]  && [[ -z $2 ]] 
-  then 
-    echo 0;
-    exit 0
+if [[ ! -z $1  &&  ! -z $2 ]] && [[ ${#1} != ${#2} ]] 
+  then
+   echo "strands must be of equal length" && exit 1
+elif [[ -z $1  &&  -z $2 ]]
+  then
+    echo "0" && exit 0 
 fi
