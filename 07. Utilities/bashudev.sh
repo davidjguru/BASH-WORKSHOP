f4cc40b73f4752035a8ce3648a095fce1b685f19
David Rodrguez <davidjguru@gmail.com>
2022-01-03 19:09:39 +0100

    (BSH) - Add a new script listening udev events.

---
+++ 07. Utilities/bashudev.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+#
+#
+#-------------------------------------------------------------------------------
+#
+# Bashudev - Registering udev events from your Linux System.
+# [David Rodríguez, @davidjguru] [davidjguru@gmail.com]
+# Title: Bashudev
+# Description: The script will be triggered by some udev event.
+# Version: v1.0.0
+#
+# davidjguru@gmail.com
+# https://davidjguru.github.io
+# https://www.therussianlullaby.com
+#-------------------------------------------------------------------------------
+#
+# Last Edited: January 03 2022
+
+# How it works
+
+## 1- Identify your device reading some output info like: $ lsblk 
+## 2- Put a copy of this script in path /usr/bin/bashudev.sh
+## 3- Open a file called 80-local.rules in /etc/udev/rules.d and enter this code:
+## SUBSYSTEM=="block", ACTION=="add", RUN+="/usr/bin/bashudev.sh"
+## 4- Restart or reload your system and plug in a new thumb drive.
+## 5- Go to review the /tmp folder and search the bashudev.log.
+date >> /tmp/bashudev.log
