ff4bf5cc0e4cc5472f0b3b57341bfc8b80c803da
David Rodrguez <davidjguru@gmail.com>
2022-01-04 19:55:59 +0100

    (BSH) - Update the udev-based script.

--- 07. Utilities/bashudev.sh
+++ 07. Utilities/bashudev.sh
@@ -19,12 +19,30 @@
 
 # How it works
 
-## Version v1.0.0 - Basic identification of a new device
+## Version v1.0.0 - Basic identification of a device
 ##------------------------------------------------------------------------------
 ## 1- Identify your device reading some output info like: $ lsblk 
 ## 2- Put a copy of this script in path /usr/bin/bashudev.sh
 ## 3- Open a file called 80-local.rules in /etc/udev/rules.d and enter code:
-## SUBSYSTEM=="block", ACTION=="add", RUN+="/usr/bin/bashudev.sh"
+##    SUBSYSTEM=="block", ACTION=="add", RUN+="/usr/bin/bashudev.sh"
 ## 4- Restart or reload your system and plug in a new thumb drive.
 ## 5- Go to review the /tmp folder and search the bashudev.log.
-date >> /tmp/bashudev.log
+
+## Version v2.0.0 - Advanced identification of a device by attributes
+##------------------------------------------------------------------------------
+## 1- Identify your device: $ lsusb
+## 2- Get vendor:product pair of keys, something like ffff:5678
+## 3- Put a copy of this script in path /usr/bin/bashudev.sh
+## 4- Open a file called 80-local.rules in /etc/udev/rules.d and enter code:
+##    SUBSYSTEM=="block", ATTRS{idVendor}=="ffff", ACTION=="add", RUN+="/usr/bin/bashudev.sh"
+## 5- Restart or reload your system and plug in a new thumb drive.
+##    you can execute something like $ sudo udevadm control --reload
+## 6- Go to review the /tmp folder and search the bashudev.log.
+## 7- Executes $ tail -f /tmp/bashudev.log and launch the file in prompt.
+## 8- Plug in / Plug out diverse thumb drives and check results in prompt.
+MSSG="Today => $(date) has been connected the selected thumb drive"
+wall $MSSG
+echo $MSSG  >> /tmp/bashudev.log
+
+
+
